7dedec61c09beaba9cf5dafa4b19b03f1e2e51d6
viniwebster <viniciuswebster99@gmail.com>
2024-08-02 15:26:12 -0300

    Creating entity Seller

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Department obj = new Department(1, "Books");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Seller seller = new Seller(25, "Vinicius", "vini@gmail", new Date(), 2000.0, obj);
+        System.out.println(seller);
     }
 }
