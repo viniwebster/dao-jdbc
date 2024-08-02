c552dc461f0ba637ba8d21b0262676d01afda533
viniwebster <viniciuswebster99@gmail.com>
2024-08-02 20:38:19 -0300

    implements findByDepartment

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        SellerDao seller = DaoFactory.createSellerDao();
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(1);
+        System.out.println("Find by id: " + seller);
+        System.out.println();
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        System.out.println("Find by department: ");
 
-        System.out.println(seller.findById(4));
+        for (Seller s : list) {
+            System.out.println(s);
+        }
     }
 }
